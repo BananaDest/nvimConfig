899881d58057c58cf0df54ab0f2caf10d2c4da29
Fabian Mena <famega12@gmail.com>
2025-05-31 20:35:16 -0600

    Updated to 0.11

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,61 +1,64 @@
 local on_attach = function(client, bufnr)
 	local bufmap = function(keys, func)
-		vim.keymap.set('n', keys, func, { buffer = bufnr })
+		vim.keymap.set("n", keys, func, { buffer = bufnr })
 	end
 
-	bufmap('<leader>r', vim.lsp.buf.rename)
-	bufmap('<leader>a', vim.lsp.buf.code_action)
+	bufmap("<leader>r", vim.lsp.buf.rename)
+	bufmap("<leader>a", vim.lsp.buf.code_action)
 
-	bufmap('gd', vim.lsp.buf.definition)
-	bufmap('gD', vim.lsp.buf.declaration)
-	bufmap('gI', vim.lsp.buf.implementation)
-	bufmap('<leader>D', vim.lsp.buf.type_definition)
+	bufmap("gd", vim.lsp.buf.definition)
+	bufmap("gD", vim.lsp.buf.declaration)
+	bufmap("gI", vim.lsp.buf.implementation)
+	bufmap("<leader>D", vim.lsp.buf.type_definition)
 
-	bufmap('gr', require('telescope.builtin').lsp_references)
-	bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
-	bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+	bufmap("gr", require("telescope.builtin").lsp_references)
+	bufmap("<leader>s", require("telescope.builtin").lsp_document_symbols)
+	bufmap("<leader>S", require("telescope.builtin").lsp_dynamic_workspace_symbols)
 
-	bufmap('K', vim.lsp.buf.hover)
+	bufmap("K", vim.lsp.buf.hover)
 
-	vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-		vim.lsp.buf.format()
+	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+		vim.lsp.buf.format({ async = false })
+		require("conform").format({ bufnr = bufnr })
 	end, {})
-	bufmap('<C-f>', ':Format<CR>')
+	bufmap("<C-f>", ":Format<CR>")
 
 	vim.api.nvim_create_autocmd("BufWritePre", {
+		buffer = bufnr,
 		callback = function()
-			vim.lsp.buf.format({ async = false })
-		end
+			require("conform").format({
+				bufnr = bufnr,
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end,
 	})
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 require("mason").setup()
-
-require("mason-lspconfig").setup_handlers({
-
-	function(server_name)
-		require("lspconfig")[server_name].setup {
-			on_attach = on_attach,
-			capabilities = capabilities,
-		}
-	end,
-
-	["lua_ls"] = function()
-		require('neodev').setup()
-		require('lspconfig').lua_ls.setup {
-			on_attach = on_attach,
-			capabilities = capabilities,
-			settings = {
-				Lua = {
-					workspace = { checkThirdParty = false },
-					telemetry = { enable = false },
-				},
-			}
-		}
-	end,
-
+require("mason-lspconfig").setup()
+local lspconfig = require("lspconfig")
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "black" },
+	},
+})
+require("neodev").setup()
+lspconfig.lua_ls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+vim.lsp.config("*", {
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+vim.lsp.enable("pyright")
+vim.lsp.config("pyright", {
+	on_attach = on_attach,
+	capabilities = capabilities,
 })
--- lua/options.lua
+++ lua/options.lua
@@ -5,14 +5,16 @@ vim.o.clipboard = 'unnamedplus'
 
 vim.o.number = true
 
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+vim.o.tabstop = 2
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
 
 vim.o.updatetime = 300
 
 vim.o.termguicolors = true
 vim.o.mouse = 'a'
-
+vim.o.relativenumber = true
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
